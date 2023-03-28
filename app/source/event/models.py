71964cbb5b9914c7933dd8cd4a48afa1b12f4a20
jo <kaitokinjo@gmail.com>
2023-03-28 21:52:07 +0900

    change init value

--- app/source/event/models.py
+++ app/source/event/models.py
@@ -5,8 +5,8 @@ from django.core.validators import RegexValidator
 class Event(models.Model):
     title = models.CharField(blank=False, null=False, max_length=150)
     text = models.TextField(blank=True)
-    start_datetime = models.DateTimeField(auto_now_add=True)
-    end_datetime = models.DateTimeField(auto_now_add=True)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
     mail = models.EmailField(max_length=240)
     tel_number_regex = RegexValidator(regex=r'^[0-9]+$', message=(
         "Tel Number must be entered in the format: '09012345678'. Up to 15 digits allowed."))
