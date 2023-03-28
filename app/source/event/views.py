fb49a8bd7ec07f1df1f152a49020b78a852dd3d7
jo <kaitokinjo@gmail.com>
2023-03-28 21:27:44 +0900

    create view

--- app/source/event/views.py
+++ app/source/event/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    resdict = {"resstr": "test"}
+    return render(request, "event/index.html", resdict)
