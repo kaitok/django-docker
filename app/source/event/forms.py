4a68cf917b29dfefffcb9b5e3ec3885862b5c4ca
jo <kaitokinjo@gmail.com>
2023-04-01 14:32:26 +0900

    create form and validation

---
+++ app/source/event/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from django.core.validators import RegexValidator
+import re
+
+from .models import Event
+
+
+class EventForm(forms.ModelForm):
+    tel_number_regex = RegexValidator(regex=r'^[0-9]+$', message=(
+        "Tel Number must be entered in the format: '09012345678'. Up to 15 digits allowed."))
+
+    tel_number = forms.CharField(
+        validators=[tel_number_regex], max_length=15, label='電話番号')
+
+    class Meta:
+        model = Event
+        fields = ['title', 'text', 'start_datetime',
+                  'end_datetime', 'mail', 'tel_number']
+        labels = {
+            'title': 'タイトル',
+            'text': '本文',
+            'start_datetime': '開始日時',
+            'end_datetime': '終了日時',
+            'mail': 'メールアドレス',
+        }
+        widgets = {
+            'start_datetime': forms.TextInput(attrs={'type': 'datetime-local'}),
+            'end_datetime': forms.TextInput(attrs={'type': 'datetime-local'}),
+        }
+
+    def is_valid_email(self, email):
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        return re.match(pattern, email) is not None
+
+    def clean_mail(self):
+        mail = self.cleaned_data['mail']
+        if not self.is_valid_email(mail):
+            raise forms.ValidationError('有効なメールアドレスを入力してください。')
+        return mail
+
+    def clean_tel_number(self):
+        tel_number = self.cleaned_data['tel_number']
+        if len(tel_number) < 10:
+            raise forms.ValidationError('電話番号は10桁以上で入力してください。')
+        return tel_number
