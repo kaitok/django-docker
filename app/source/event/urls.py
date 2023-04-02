1d216c7585c88c1d92c31850d406e607dcd765e7
jo <kaitokinjo@gmail.com>
2023-04-02 20:51:37 +0900

    Add event link

--- app/source/event/urls.py
+++ app/source/event/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+app_name = "event"
 urlpatterns = [
     path("", views.indexView, name="event-index"),
     path("create/", views.createView, name="event-create"),
