45ee7f7f9c2b56b1b46100777a3b0dc2921fe90f
jo <kaitokinjo@gmail.com>
2023-04-07 22:19:18 +0900

    add test

--- app/source/event/tests.py
+++ app/source/event/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .forms import EventForm
 
-# Create your tests here.
+class EventFormTestCase(TestCase):
+    def test_valid_data(self):
+        form_data = {
+            'title': 'テストイベント',
+            'text': 'テストイベントの説明',
+            'start_datetime': '2023-04-07T13:51',
+            'end_datetime': '2023-04-07T14:51',
+            'mail': 'test@example.com',
+            'tel_number': '09012345678',
+        }
+
+        form = EventForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_data(self):
+        form_data = {
+            'title': 'テストイベント',
+            'text': 'テストイベントの説明',
+            'start_datetime': '2023-04-07T14:51',
+            'end_datetime': '2023-04-07T13:51',
+            'mail': 'test@example.com',
+            'tel_number': 'invalid',
+        }
+
+        form = EventForm(data=form_data)
+        self.assertFalse(form.is_valid())
